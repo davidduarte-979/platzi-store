9a7c1ecd75f18785fa3c7d1530cc6da7bf0d6b6c
Jesus Duarte <jduartedsp@gmail.com>
2024-04-16 06:21:03 -0400

    query, params, controllers, services

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,37 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('new')
+  newEndpoint(): string {
+    return "I'm new endpoint";
+  }
+
+  @Get('products/filter')
+  getProductFilter() {
+    return `products filter 1`;
+  }
+
+  @Get('products/:productId')
+  getProduct(@Param('productId') productId: string) {
+    return `Product ${productId}`;
+  }
+
+  @Get('categories/:id/products/:productId')
+  getCategories(
+    @Param('productId') productId: string,
+    @Param('id') id: string,
+  ) {
+    return `Product ${productId} category ${id}`;
+  }
+
+  @Get('products')
+  getProducts(
+    @Query('limit') limit = 100,
+    @Query('offset') offset = 0,
+    @Query('brand') brand: string,
+  ) {
+    return `products limit= => ${limit} product offset => ${offset} product brand => ${brand}`;
   }
 }
